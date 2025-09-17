2e172aef2fc97fbc3ee69751c3a00d3b435f42e4
grimbakor <davidstur@gmail.com>
2025-09-17 20:45:20 +0100

    working multi-upload with multiple loading bars

--- backend/src/run.sh
+++ backend/src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx nodemon --ext "go,html" --signal SIGTERM --exec go run *.go -- -f $HOME -verbose -t 300 -uploads
+npx nodemon --ext "go,html" --signal SIGTERM --exec "(rm ~/test/*) & go run *.go" -- -f $HOME -verbose -t 300 -uploads
